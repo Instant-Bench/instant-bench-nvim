4f0570a10c7dc8b47b3f4f52d98b116687690ff9
RafaelGSS <rafael.nunu@hotmail.com>
2024-07-01 00:36:28 -0300

    fix: use plenary.curl instead of requests module

--- lua/instant-bench/init.lua
+++ lua/instant-bench/init.lua
@@ -1,15 +1,30 @@
 local M = {}
 
 local function makeHttpRequest(url, data)
-    local requests = require("requests")
-    local reponse = requests.post{url, data}
-    return response
+    local plenary_http = require("plenary.curl")
+    local json = vim.fn.json_encode(data)
+
+    local response = plenary_http.post(url, {
+        body = json,
+        headers = {
+            ["Content-Type"] = "application/json",
+        },
+    })
+
+    if response.status == 200 then
+        return {
+            status_code = response.status,
+            text = response.body
+        }
+    else
+        return nil, "HTTP request failed with status: " .. response.status
+    end
 end
 
 local function getSelectedText()
     -- Save current register contents and visual mode
     local saved_register = vim.fn.getreg('"')
-    local saved_visual_mode = vim.fn.visualmode()
+    local saved_visual_mode = vim.fn.mode()
 
     -- Enter visual mode and yank selection to @" register
     vim.cmd('normal! gv"xy')
@@ -19,7 +34,7 @@ local function getSelectedText()
 
     -- Restore original register contents and visual mode
     vim.fn.setreg('"', saved_register)
-    vim.fn.visualmode(saved_visual_mode)
+    vim.api.nvim_feedkeys(saved_visual_mode, 'x', true)
 
     return selected_text
 end
@@ -29,14 +44,18 @@ function M.sendSelectedText()
     print("Selected:", selected_text)
     print("File type:", vim.bo.filetype)
     local endpoint_url = "http://localhost:3000/"
-    local res, code, headers, body = makeHttpRequest(endpoint_url, {"extension": vim.bo.filetype, "code": selected_text})
+    local response, err = makeHttpRequest(endpoint_url, { extension = vim.bo.filetype, code = selected_text })
 
-    if code == 200 then
-        print("HTTP request successful!")
-        print("Response Body:", body)
+    if response then
+        if response.status_code == 200 then
+            print("HTTP request successful!")
+            print("Response Body:", response.text)
+        else
+            print("HTTP request failed. Error code:", response.status_code)
+            print("Response Body:", response.text)
+        end
     else
-        print("HTTP request failed. Error code:", code)
-        print("Response Body:", body)
+        print("HTTP request failed. Error:", err)
     end
 end
 
